66a92d8a144425d4338db7ff658c809558f06162
Kuruva Rambabu <r151763@rguktrkv.ac.in>
2023-03-06 11:05:14 +0530

    remove unnecessary files

--- src/hooks/getJobsHook.ts
+++ src/hooks/getJobsHook.ts
@@ -4,11 +4,11 @@ import { JobDataResponseObj } from "../stores/types";
 import { useJobsService } from "./jobsServiceHook";
 import StoresContext from "./storeHook";
 
-export const useGetJobsAPI = (employementFilters?: Array<string>, salaryRangeFilter?: string | undefined, searchInput?: string | undefined)=>{
+export const useGetJobsAPI = (employementFilters?: Array<string>, salaryRangeFilter?: string, searchInput?: string)=>{
 const stores = useContext(StoresContext)
 const jobService = useJobsService()
 const {jobStore} = stores
-    return useQuery<any |JobDataResponseObj,any,any>(["JobsData", employementFilters, salaryRangeFilter, searchInput],
+    return useQuery<unknown |JobDataResponseObj,any,any>(["JobsData", employementFilters, salaryRangeFilter, searchInput],
     () => jobService.getJobsAPI(employementFilters, salaryRangeFilter, searchInput),{
 
     onSuccess: (data)=>{

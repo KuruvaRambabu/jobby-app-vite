89619ab3e18d457ee5b5b32d3cb2d797aaf8460d
Kuruva Rambabu <r151763@rguktrkv.ac.in>
2023-03-09 20:56:32 +0530

    login playwright tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,11 +3,13 @@ import { test, expect } from '@playwright/test';
 test('Login Page Tests', async ({ page }) => {
     await page.goto('http://localhost:5173/');
     await page.pause()
+    await page.getByPlaceholder('Password').press('Enter');
+    await expect(page.getByText('Username or password is invalid')).toBeVisible()
+    
     await page.getByPlaceholder("Username").fill("rahul")
     await page.getByPlaceholder("Password").fill("rahul@2021")
     await page.getByPlaceholder('Password').press('Enter');
-    const texts = await page.getByRole('button');
-    await expect(texts).toHaveText("Logout")
+    await expect(page.getByRole('button')).toHaveText("Logout")
     
   });
 
